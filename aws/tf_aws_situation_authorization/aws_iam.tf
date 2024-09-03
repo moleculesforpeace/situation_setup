cd8aee32d5581086316809b3d994e11ab8706aab
Alan Zanatta <alan@moleculesforpeace.com>
2024-09-03 09:21:28 +0200

    Adds AWS Organization support.

--- aws/tf_aws_situation_authorization/aws_iam.tf
+++ aws/tf_aws_situation_authorization/aws_iam.tf
@@ -1,10 +1,34 @@
+resource "aws_iam_policy" "assume_role_on_other_accounts_from_org" {
+  name        = "AssumeRoleOnOrganizationAccounts"
+  description = "Enables sts:AssumeRole on AWS Accounts from the same Organization"
+  path        = "/situation/"
+  policy = jsonencode(
+    {
+      "Version" = "2012-10-17"
+      "Statement" = [{
+        "Sid"      = "AssumeRole"
+        "Effect"   = "Allow"
+        "Action"   = "sts:AssumeRole"
+        "Resource" = "arn:aws:iam::*:role/OrganizationAccountAccessRole"
+        "Condition" = {
+          "StringEquals" = {
+            "aws:ResourceOrgID" = "$${aws:PrincipalOrgID}"
+          }
+        }
+      }]
+    }
+  )
+}
+
 resource "aws_iam_role" "cspm" {
   name = "CloudSecurityPostureManagement"
+  path = "/situation/"
   managed_policy_arns = [
     "arn:aws:iam::aws:policy/SecurityAudit",
     "arn:aws:iam::aws:policy/AWSSSOReadOnly",
     "arn:aws:iam::aws:policy/IAMReadOnlyAccess",
-    "arn:aws:iam::aws:policy/AWSOrganizationsReadOnlyAccess"
+    "arn:aws:iam::aws:policy/AWSOrganizationsReadOnlyAccess",
+    aws_iam_policy.assume_role_on_other_accounts_from_org.arn,
   ]
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
